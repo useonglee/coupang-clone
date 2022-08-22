83c47edb95bee828f394641489e9df209210beaa
useonglee <useong0830@naver.com>
2022-08-22 23:58:20 +0900

    #2 feat: next.js image 외부 도메인 주소 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "thumbnail6.coupangcdn.com",
+      "thumbnail7.coupangcdn.com",
+      "thumbnail8.coupangcdn.com",
+      "thumbnail9.coupangcdn.com",
+      "thumbnail10.coupangcdn.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
