4e4a64ec0476b9a26de5fa501161193596c4e0c9
useonglee <useong0830@naver.com>
2022-08-24 00:00:42 +0900

    2 feat: 상품 목록 리스트 데이터 타입 적용

--- src/hooks/useProductList.ts
+++ src/hooks/useProductList.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "react-query";
 import ProductService from "@service/product.service";
+import { IProductList } from "@/types/products";
 
-const useProductList = (queryString: string) => {
+const useProductList = (queryString: string): IProductList => {
   const { data: productListData } = useQuery(
     ["product-list", queryString],
     () => ProductService.fetchProductData(queryString),
