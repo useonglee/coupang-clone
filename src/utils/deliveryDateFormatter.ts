c51e35c10d9398d246fc3f15aba5f14c815282b5
useonglee <useong0830@naver.com>
2022-08-23 23:59:32 +0900

    #2 feat: 상품 배송 도착 시간 계산 유틸 함수 구현

---
+++ src/utils/deliveryDateFormatter.ts
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+dayjs.locale("ko");
+
+const diffChecker = (date: string, day: number) => {
+  const today = dayjs().get("D");
+  const formattedDate = dayjs(date).format("dd");
+
+  const dayDiff = day - today;
+
+  if (dayDiff === 1) {
+    return `내일(${formattedDate})`;
+  }
+
+  if (dayDiff === 2) {
+    return `모레(${formattedDate})`;
+  }
+
+  return `${formattedDate}요일`;
+};
+
+const deliveryDateFormatter = (date: string, isAssured: boolean) => {
+  const deliveryMonth = dayjs(date).get("M") + 1;
+  const deliveryDay = dayjs(date).get("D");
+  const dateResult = diffChecker(date, deliveryDay);
+
+  if (isAssured) {
+    return dateResult;
+  }
+
+  return `${dateResult} ${deliveryMonth}/${deliveryDay}`;
+};
+
+export default deliveryDateFormatter;
