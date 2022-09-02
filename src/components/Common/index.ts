5a41a014606b3545d9fd6dd42917e302f2f89619
useonglee <useong0830@naver.com>
2022-09-02 11:16:16 +0900

    #16 feat: Spinner 컴포넌트 export 모듈 파일에 추가

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -3,3 +3,4 @@ export { default as Layout } from "./Layout/Layout";
 export { default as Pagination } from "./Pagination/Pagination";
 export { default as Radio } from "./Radio/Radio";
 export { default as Select } from "./Select/Select";
+export { default as Spinner } from "./Spinner/Spinner";
