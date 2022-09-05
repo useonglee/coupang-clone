d7fdc6a17732e8001a0229dea418c05136e2aed0
useonglee <useong0830@naver.com>
2022-09-05 12:36:47 +0900

    feat: nextjs 첫 페이지 리디렉팅 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/cart",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
