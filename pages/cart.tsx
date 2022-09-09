28d10e53bc28121aca5a37ee4c101c77f292f36f
useonglee <useong0830@naver.com>
2022-09-09 23:13:02 +0900

    #24 feat: login api를 통해 토큰 발급 기능 구현

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,3 +1,19 @@
-export default function CartPage() {
-  return <></>;
-}
+import { useEffect } from "react";
+import useAuth from "@hooks/useAuth";
+import cookies from "js-cookie";
+
+const CartPage = () => {
+  const getAuthTokens = useAuth();
+
+  useEffect(() => {
+    if (cookies.get("accessToken")) {
+      return;
+    }
+
+    getAuthTokens({ email: "useong0830@naver.com", password: "1234" });
+  }, []);
+
+  return <div>cart</div>;
+};
+
+export default CartPage;
