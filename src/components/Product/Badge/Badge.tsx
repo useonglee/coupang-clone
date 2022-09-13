7b490493b273992c9e8c9a877c74d34e49b7176b
useonglee <useong0830@naver.com>
2022-09-13 21:21:38 +0900

    #26 feat: 적립 뱃지 UI 컴포넌트 구현

---
+++ src/components/Product/Badge/Badge.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import * as Style from "./Badge.style";
+
+interface IBadgeProps {
+  maxPoint: number;
+}
+
+const Badge = ({ maxPoint }: IBadgeProps) => {
+  return (
+    <Style.BadgeContainer>
+      <Image
+        src="https://image6.coupangcdn.com/image/badges/cashback/web/list-cash-icon@2x.png"
+        alt={`상품 최대 적립 포인트 ${maxPoint}원`}
+        width={14}
+        height={14}
+      />
+      <span>{`최대 ${maxPoint}원 적립`}</span>
+    </Style.BadgeContainer>
+  );
+};
+
+export default Badge;
