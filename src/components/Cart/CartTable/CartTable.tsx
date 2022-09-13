9b7cc0e6b5264f139482915fb037cb02de8822f3
useonglee <useong0830@naver.com>
2022-09-13 21:31:23 +0900

    #26 feat: CartList 컴포넌트 삭제로 인한 import 제거

--- src/components/Cart/CartTable/CartTable.tsx
+++ src/components/Cart/CartTable/CartTable.tsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import { CartItemList } from "@components/Cart";
 import useCartItemList from "@components/Cart/hooks/useCartItemList";
 import * as Style from "./CartTable.style";
 
