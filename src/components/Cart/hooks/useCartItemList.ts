e609b1f7f5b3b20601ec512ebb720888dd755b79
useonglee <useong0830@naver.com>
2022-09-15 21:00:58 +0900

    #34 feat: 카트 아이템 목록 조회 hook 함수 signal 기능 적용

--- src/components/Cart/hooks/useCartItemList.ts
+++ src/components/Cart/hooks/useCartItemList.ts
@@ -1,22 +1,30 @@
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import CartService from "@services/cart.service";
 import { ICartItemListData } from "@components/Cart/types/cart.type";
+import queryKey from "../constants/queryKey";
 import cookies from "js-cookie";
 
-const useCartItemList = (): ICartItemListData[] => {
-  const { data: cartItemListData } = useQuery(
-    ["cart-list"],
-    () => CartService.fetchCartList(),
+interface IUseCartItemList {
+  cartItemList: ICartItemListData[];
+  updateCartItemList: (cartItemList: any) => void;
+}
+
+const useCartItemList = (): IUseCartItemList => {
+  const queryClient = useQueryClient();
+  const [cartItemList, setCartItemList] = useState<ICartItemListData[]>([]);
+
+  const { data = [] } = useQuery(
+    [queryKey.cart],
+    ({ signal }) => CartService.fetchCartList(signal),
     {
       enabled: !!cookies.get("accessToken"),
-      select: (data) => {
+      onSuccess: (data) => {
         const processProductPrice = data.map((product: ICartItemListData) => {
           const { salePrice, maxPoint } = product.product;
 
           product.product.salePrice = salePrice.toLocaleString();
           product.product.maxPoint = maxPoint.toLocaleString();
-
-          return product;
         });
 
         return processProductPrice;
@@ -24,7 +32,18 @@ const useCartItemList = (): ICartItemListData[] => {
     }
   );
 
-  return cartItemListData;
+  const updateCartItemList = (cartItemList: ICartItemListData[]) => {
+    setCartItemList(cartItemList);
+    queryClient.setQueryData([queryKey.cart], cartItemList);
+  };
+
+  useEffect(() => {
+    if (data.length > 0) {
+      setCartItemList(data);
+    }
+  }, [data, cartItemList]);
+
+  return { cartItemList, updateCartItemList };
 };
 
 export default useCartItemList;
