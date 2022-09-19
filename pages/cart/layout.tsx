5aaf0c58dffd5dadb1cca379d85f4321d0140813
useonglee <useong0830@naver.com>
2022-09-19 19:26:59 +0900

    #38 feat: 카트 아이템 데이터 업데이트 시 Spinner UI 노출

--- pages/cart/layout.tsx
+++ pages/cart/layout.tsx
@@ -1,18 +1,24 @@
 import { ReactNode } from "react";
 import Head from "next/head";
+import { Spinner } from "@components/Cart";
 import styled from "@emotion/styled";
+import { useRecoilValue } from "recoil";
+import { cartSpinnerAtom } from "@components/Cart/recoil/spinner";
 
 interface ICartPageLayoutProps {
   children: ReactNode;
 }
 
 const CartPageLayout = ({ children }: ICartPageLayoutProps) => {
+  const cartSpinner = useRecoilValue(cartSpinnerAtom);
+
   return (
     <>
       <Head>
         <title>쿠팡! | 장바구니</title>
       </Head>
       <CartPageLayoutStyle>{children}</CartPageLayoutStyle>
+      {cartSpinner && <Spinner.Clip />}
     </>
   );
 };
