f4f8fa07e13f74910d9b9b7cfda705e633836453
useonglee <useong0830@naver.com>
2022-09-21 00:07:51 +0900

    #36 feat: 장바구니 아이템 전체선택 체크박스 적용 및 테이블 주문 관련 OrderFoot UI 컴포넌트 적용

--- src/components/Cart/CartTable/CartTable.tsx
+++ src/components/Cart/CartTable/CartTable.tsx
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
+import { Checkbox } from "@components/Common";
 import { RocketProduct, SellerProduct } from "@components/Cart";
+import OrderFoot from "./OrderFoot/OrderFoot";
 import { ICartItemListData } from "../types/cart.type";
 import * as Style from "./CartTable.style";
 
@@ -23,6 +25,12 @@ const CartTable = ({ cartItemList }: ICartTableProps) => {
     return { rocketProductList, sellerProductList };
   }, [cartItemList]);
 
+  const cartIdList = (): number[] => {
+    const itemNameArray = cartItemList.map(({ id }) => id);
+
+    return itemNameArray;
+  };
+
   return (
     <Style.CartTableContainer>
       <caption>장바구니</caption>
@@ -33,18 +41,28 @@ const CartTable = ({ cartItemList }: ICartTableProps) => {
         <col width="90" />
         <col width="90" />
       </colgroup>
-      <thead>
-        <Style.CartTableHeaderRow>
-          <th scope="col">{cartItemList.length > 0 && "전체선택"}</th>
-          <th scope="colgroup" colSpan={2}>
-            상품정보
-          </th>
-          <th scope="col">상품금액</th>
-          <th scope="col">배송비</th>
-        </Style.CartTableHeaderRow>
-      </thead>
-      <RocketProduct cartItemList={cart.rocketProductList} />
-      <SellerProduct cartItemList={cart.sellerProductList} />
+      <Checkbox.Group>
+        <thead>
+          <Style.CartTableHeaderRow>
+            <Style.AllCheckItemTh scope="col">
+              {cartItemList.length > 0 && (
+                <Checkbox.AllCheckItem label="전체선택" list={cartIdList()} />
+              )}
+            </Style.AllCheckItemTh>
+            <th scope="colgroup" colSpan={2}>
+              상품정보
+            </th>
+            <th scope="col">상품금액</th>
+            <th scope="col">배송비</th>
+          </Style.CartTableHeaderRow>
+        </thead>
+        <RocketProduct cartItemList={cart.rocketProductList} />
+        <SellerProduct cartItemList={cart.sellerProductList} />
+        <OrderFoot
+          cartIdList={cartIdList()}
+          cartItemCount={cartItemList?.length}
+        />
+      </Checkbox.Group>
     </Style.CartTableContainer>
   );
 };
