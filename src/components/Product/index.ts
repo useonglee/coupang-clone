0fe972ec93384bdc2e748928a2a66e72f35b189c
useonglee <useong0830@naver.com>
2022-09-22 23:22:32 +0900

    fix: stage4 -> main merge 상태에서 git 충돌 해결

--- src/components/Product/index.ts
+++ src/components/Product/index.ts
@@ -1,2 +1,5 @@
 export { default as Badge } from "./Badge/Badge";
 export { default as DeliveryDate } from "./DeliveryDate/DeliveryDate";
+export { default as CoupangBadge } from "./CoupangBadge/CoupangBadge";
+export { default as ProductList } from "./ProductList/ProductList";
+export { default as ProductPrice } from "./ProductPrice/ProductPrice";
