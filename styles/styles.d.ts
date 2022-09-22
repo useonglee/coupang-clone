51e6f7113ca8048f4ee7577cce14a53b4bd83c7d
useonglee <useong0830@naver.com>
2022-09-22 23:20:30 +0900

    fix: git 충돌 해결

--- styles/styles.d.ts
+++ styles/styles.d.ts
@@ -4,6 +4,15 @@ declare module "@emotion/react" {
   export interface DefaultTheme {
     color: {
       primary: string;
+      secondary: string;
+      grayCC: string;
+      lightGray: string;
+      basicGray: string;
+      darkGray: string;
+      priceColor: string;
+      boxShadow: string;
+      black: string;
+      black300: string;
     };
   }
 }
