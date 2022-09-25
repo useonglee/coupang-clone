22ddef7af436e3e09099f34820d215dcba33414f
useonglee <useong0830@naver.com>
2022-09-25 16:46:58 +0900

    #53 feat: 장바구니 페이지 컴포넌트 하단 버튼 스타일 추가 및 유저 정보를 통해 주문 페이지 이동

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -10,8 +10,10 @@ import {
 } from "@components/Cart";
 import { Button, CustomSuspense, Layout } from "@components/@shared";
 import useCartItemList from "@components/Cart/hooks/useCartItemList";
+import useUser from "@hooks/useUser";
 import { useRecoilValue } from "recoil";
 import { rocketPriceAtom, sellerPriceAtom } from "@recoil/totalPrice";
+import styled from "@emotion/styled";
 
 interface ICartPageProps {
   authToken: string;
@@ -21,6 +23,7 @@ const CartPage = ({ authToken }: ICartPageProps) => {
   const [isUserLogin, setIsUserLogin] = useState<boolean>(!!authToken);
 
   const { cartItemList } = useCartItemList();
+  const userInfo = useUser();
 
   const rocketTotalPrice = useRecoilValue(rocketPriceAtom);
   const sellerTotalPrice = useRecoilValue(sellerPriceAtom);
@@ -33,7 +36,7 @@ const CartPage = ({ authToken }: ICartPageProps) => {
 
   return (
     <CustomSuspense fallback={<Spinner.Beat />}>
-      <Layout title="쿠팡! | 장바구니">
+      <PageLayout title="쿠팡! | 장바구니">
         <section>
           <CartTable cartItemList={cartItemList} />
         </section>
@@ -45,7 +48,7 @@ const CartPage = ({ authToken }: ICartPageProps) => {
             setIsUserLogin={setIsUserLogin}
           />
         )}
-        <div>
+        <GoAnotherPageButtonArea>
           <Link href="https://www.coupang.com/">
             <a>
               <Button variant="outlined" size="medium" shape="round">
@@ -53,15 +56,15 @@ const CartPage = ({ authToken }: ICartPageProps) => {
               </Button>
             </a>
           </Link>
-          <Link href={`cart/checkout/${1}`}>
+          <Link href={`cart/checkout/${userInfo?.id}`}>
             <a>
               <Button variant="primary" size="medium" shape="round">
                 구매하기
               </Button>
             </a>
           </Link>
-        </div>
-      </Layout>
+        </GoAnotherPageButtonArea>
+      </PageLayout>
     </CustomSuspense>
   );
 };
@@ -74,5 +77,19 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: { authToken },
   };
 };
+
+const PageLayout = styled(Layout)`
+  width: 89.8rem;
+  margin: 0 auto;
+`;
+
+const GoAnotherPageButtonArea = styled.div`
+  margin-top: 5rem;
+  text-align: center;
+
+  & > a:nth-of-type(1) {
+    margin-right: 2rem;
+  }
+`;
 
 export default CartPage;
